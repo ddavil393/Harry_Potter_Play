c47037a3308e36b291e88717ba440920ba05b62b
ddavil393 <memv.cm@gmail.com>
2024-03-10 01:00:25 -0600

    Cambios es pantalla user, fondo, letra y boton en Index start

--- JS/P_index.js
+++ JS/P_index.js
@@ -1,3 +1,10 @@
 function PlayAudio(){
     document.getElementById("Music").play();
 }
+
+//Si el nombre de user esta registrado mostrar mejor tiempo y puntaje anterior
+//Esto cuando toque el boton de enviar
+
+
+
+//Una vez que no que toque enviar  
